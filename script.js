11a8e1239d43322bee4afe58f7efc59b5fac0d25
badsha-sheikh <badshasheikhdeveloper@gmail.com>
2025-08-29 13:03:31 +0600

    addning functionalities of call button primary

--- script.js
+++ script.js
@@ -11,11 +11,239 @@ for (const icon of icons){
      
 }
 // Call Button 
-const callButtons = document.getElementsByClassName("call");
-for(const callButton of callButtons){
-    callButton.addEventListener("click",function(){
-    const serviceName = document.getElementById("service-name").innerText;
-    const serviceNumber = document.getElementById("service-number").innerText;
-    console.log(serviceName,serviceNumber)
-    })
+//function to get button by id
+function getCallButton(id){
+   let button =  document.getElementById(id)
+   return button
+};
+// function to get all name
+function getName(){
+    let names = document.getElementsByClassName("service-name")
+    for (let name of names){
+        let nameText = name.innerText 
+        
+    }
 }
+getName()
+//card 1
+getCallButton("call-button-national")
+.addEventListener("click",function(){
+    console.log("clikced")
+     const serviceName = document.getElementById("service-name-national").innerText;
+ const serviceNumber = document.getElementById("service-number-national").innerText;
+ const alertMessage = ["Calling" + " " + serviceName +  " " + serviceNumber ];
+     let availableCoin = parseInt(document.getElementById("available-coin").innerText);
+   
+ if( availableCoin >= 20){
+    alert(alertMessage);
+   availableCoin = availableCoin - 20;
+document.getElementById("available-coin").innerText = availableCoin;
+ }else {
+     alert ("Not enough coins to make a call!")
+}
+})
+
+ 
+
+
+
+
+//card 2
+let policeCallButton = getCallButton("call-button-police")
+.addEventListener("click",function(){
+    console.log("clikced")
+     const serviceName = document.getElementById("service-name-police").innerText;
+ const serviceNumber = document.getElementById("service-number-police").innerText;
+ const alertMessage = ["Calling" + " " + serviceName +  " " + serviceNumber ];
+     let availableCoin = parseInt(document.getElementById("available-coin").innerText);
+   
+ if( availableCoin >= 20){
+    alert(alertMessage);
+   availableCoin = availableCoin - 20;
+document.getElementById("available-coin").innerText = availableCoin;
+ }else {
+     alert ("Not enough coins to make a call!")
+}
+})
+//card 3
+getCallButton("call-button-fire")
+.addEventListener("click",function(){
+    console.log("clikced")
+     const serviceName = document.getElementById("service-name-fire").innerText;
+ const serviceNumber = document.getElementById("service-number-fire").innerText;
+ const alertMessage = ["Calling" + " " + serviceName +  " " + serviceNumber ];
+     let availableCoin = parseInt(document.getElementById("available-coin").innerText);
+   
+ if( availableCoin >= 20){
+    alert(alertMessage);
+   availableCoin = availableCoin - 20;
+document.getElementById("available-coin").innerText = availableCoin;
+ }else {
+     alert ("Not enough coins to make a call!")
+}
+})
+//card 4
+ getCallButton("call-button-ambulance")
+.addEventListener("click",function(){
+    console.log("clikced")
+     const serviceName = document.getElementById("service-name-ambulance").innerText;
+ const serviceNumber = document.getElementById("service-number-ambulance").innerText;
+ const alertMessage = ["Calling" + " " + serviceName +  " " + serviceNumber ];
+     let availableCoin = parseInt(document.getElementById("available-coin").innerText);
+   
+ if( availableCoin >= 20){
+    alert(alertMessage);
+   availableCoin = availableCoin - 20;
+document.getElementById("available-coin").innerText = availableCoin;
+ }else {
+     alert ("Not enough coins to make a call!")
+}
+})
+//card 5
+ getCallButton("call-button-women-child")
+.addEventListener("click",function(){
+    console.log("clikced")
+     const serviceName = document.getElementById("service-name-women-child").innerText;
+ const serviceNumber = document.getElementById("service-number-women-child").innerText;
+ const alertMessage = ["Calling" + " " + serviceName +  " " + serviceNumber ];
+     let availableCoin = parseInt(document.getElementById("available-coin").innerText);
+   
+ if( availableCoin >= 20){
+    alert(alertMessage);
+   availableCoin = availableCoin - 20;
+document.getElementById("available-coin").innerText = availableCoin;
+ }else {
+     alert ("Not enough coins to make a call!")
+}
+})
+//card 6
+ getCallButton("call-button-corruption")
+.addEventListener("click",function(){
+    console.log("clikced")
+     const serviceName = document.getElementById("service-name-corruption").innerText;
+ const serviceNumber = document.getElementById("service-number-corruption").innerText;
+ const alertMessage = ["Calling" + " " + serviceName +  " " + serviceNumber ];
+     let availableCoin = parseInt(document.getElementById("available-coin").innerText);
+   
+ if( availableCoin >= 20){
+    alert(alertMessage);
+   availableCoin = availableCoin - 20;
+document.getElementById("available-coin").innerText = availableCoin;
+ }else {
+     alert ("Not enough coins to make a call!")
+}
+})
+//card 7
+ getCallButton("call-button-electricity")
+.addEventListener("click",function(){
+    console.log("clikced")
+     const serviceName = document.getElementById("service-name-electricity").innerText;
+ const serviceNumber = document.getElementById("service-number-electricity").innerText;
+ const alertMessage = ["Calling" + " " + serviceName +  " " + serviceNumber ];
+     let availableCoin = parseInt(document.getElementById("available-coin").innerText);
+   
+ if( availableCoin >= 20){
+    alert(alertMessage);
+   availableCoin = availableCoin - 20;
+document.getElementById("available-coin").innerText = availableCoin;
+ }else {
+     alert ("Not enough coins to make a call!")
+}
+})
+//card 8
+getCallButton("call-button-brac")
+.addEventListener("click",function(){
+    console.log("clikced")
+     const serviceName = document.getElementById("service-name-brac").innerText;
+ const serviceNumber = document.getElementById("service-number-brac").innerText;
+ const alertMessage = ["Calling" + " " + serviceName +  " " + serviceNumber ];
+     let availableCoin = parseInt(document.getElementById("available-coin").innerText);
+   
+ if( availableCoin >= 20){
+    alert(alertMessage);
+   availableCoin = availableCoin - 20;
+document.getElementById("available-coin").innerText = availableCoin;
+ }else {
+     alert ("Not enough coins to make a call!")
+}
+})
+//card 9
+ getCallButton("call-button-railway")
+.addEventListener("click",function(){
+    console.log("clikced")
+     const serviceName = document.getElementById("service-name-railway").innerText;
+ const serviceNumber = document.getElementById("service-number-railway").innerText;
+ const alertMessage = ["Calling" + " " + serviceName +  " " + serviceNumber ];
+     let availableCoin = parseInt(document.getElementById("available-coin").innerText);
+   
+ if( availableCoin >= 20){
+    alert(alertMessage);
+   availableCoin = availableCoin - 20;
+document.getElementById("available-coin").innerText = availableCoin;
+ }else {
+     alert ("Not enough coins to make a call!")
+}
+})
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// function getServiceName () {
+//  let serviceNames = document.getElementsByClassName("service-name");
+//     for (const name of serviceNames){
+//         console.log(name.innerText)
+//     }
+// }
+
+// const callButtons = document.getElementsByClassName("call");
+
+// for(const callButton of callButtons){
+//     callButton.addEventListener("click",function(){
+   
+//     let serviceNumbers = document.getElementsByClassName("service-number");
+//     for(const number of serviceNumbers){
+//         console.log(number.innerText)
+//     }
+//     // const serviceName = document.getElementById("service-name").innerText;
+//     // const serviceNumber = document.getElementById("service-number").innerText;
+//     // const alertMessage = ["Calling" + " " + serviceName +  " " + serviceNumber ];
+//     let availableCoin = parseInt(document.getElementById("available-coin").innerText);
+   
+//     // if( availableCoin >= 20){
+//     //     alert("alertMessage");
+//     //     availableCoin = availableCoin - 20;
+//     //     document.getElementById("available-coin").innerText = availableCoin;
+//     // }else {
+//     //     alert ("Not enough coins to make a call!")
+//     // }
+
+    
+//     })
+// }
